0ecc49657dda0ac21a5c6a1847af7218ec9afa5f
Gourav Dey <gourav.dey.ece26@heritageit.edu.in>
2023-12-30 23:44:22 +0530

    Added Map Generator

--- src/com/gdgouravdey/brick/breaker/Gameplay.java
+++ src/com/gdgouravdey/brick/breaker/Gameplay.java
@@ -1,4 +1,40 @@
 package com.gdgouravdey.brick.breaker;
 
-public class Gameplay {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+    private boolean play = false;
+    private int score= 0;
+    private  int totalBricks = 21;
+    private Timer timer;
+    private int delay = 0;
+    private int playerX = 310;
+    private int ballPosX = 120;
+    private int ballPosY = 350;
+    private int ballXDir = -1;
+    private int ballYDir = -2;
+    private MapGenerator map;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
--- src/com/gdgouravdey/brick/breaker/MapGenerator.java
+++ src/com/gdgouravdey/brick/breaker/MapGenerator.java
@@ -20,9 +20,15 @@ public class MapGenerator {
         for(int i=0; i<map.length; i++) {
             for(int j=0; j<map[0].length; j++) {
                 if(map[i][j] > 0) {
-                    
+                    g.setColor(Color.WHITE);
+                    g.fillRect(j*brickWidth+80, i*brickHeight+50, brickWidth, brickHeight);
+                    g.setStroke(new BasicStroke());
+                    g.drawRect(j*brickWidth+80, i*brickHeight+50, brickWidth, brickHeight);
                 }
             }
         }
+    }
+    public void setBrickValue(int value, int row, int col) {
+        map[row][col] = value;
     }
 }
