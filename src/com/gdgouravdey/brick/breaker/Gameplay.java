70455489764ee079a229131e31f6b8640c1ccb09
Gourav Dey <gourav.dey.ece26@heritageit.edu.in>
2024-01-03 23:38:48 +0530

    Added Gameplay elements

--- src/com/gdgouravdey/brick/breaker/Gameplay.java
+++ src/com/gdgouravdey/brick/breaker/Gameplay.java
@@ -1,6 +1,7 @@
 package com.gdgouravdey.brick.breaker;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -18,9 +19,51 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private int ballXDir = -1;
     private int ballYDir = -2;
     private MapGenerator map;
+
+    public Gameplay() {
+        map = new MapGenerator(ballPosX,ballPosY);
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay,this);
+        timer.start();
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(1, 1, 692, 595);
+        map.draw((Graphics2D)g);
+        g.fillRect(0,0,3,592);
+        g.fillRect(0,0,692,3);
+        g.fillRect(691,0,3,592);
+
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("serif",Font.BOLD,25));
+        g.drawString(""+score,590,30);
+
+        g.setColor(Color.YELLOW);
+        g.fillRect(playerX,550,100,8);
+
+        g.setColor(Color.GREEN);
+        g.fillOval(ballPosX,ballPosY,20,20);
+
+        if(ballPosY>570) {
+            play=false;
+            ballPosX=0;
+            ballYDir=0;
+            g.setColor(Color.RED);
+            g.setFont(new Font("serif",Font.BOLD,30));
+            g.drawString("Game Over! Score: "+score,190,300);
+
+            g.setFont(new Font("serif",Font.BOLD,30));
+            g.drawString("Press Enter to Restart ",190,340);
+        }
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        throw new UnsupportedOperationException("Not Supported yet.");
     }
 
     @Override
@@ -30,11 +73,21 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        throw new UnsupportedOperationException("Not Supported yet.");
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    public void moveRight() {
+        play = true;
+        playerX += 20;
+    }
+
+    public void moveLeft() {
+        play = true;
+        playerX -= 20;
     }
 }
